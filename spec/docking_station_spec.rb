2e433a0084f19676b63dc3866e4ef776d49a42c7
Asuka Ochi <asukaochi@gmail.com>
2016-11-08 18:16:38 +0000

    proccess of ex 10

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,10 +1,13 @@
 require 'docking_station'
+require 'bike'
 
 RSpec.describe DockingStation do
-
 it {is_expected.to respond_to(:release_bike)}
+end
 
-
+RSpec.describe :release_bike do
+  it {is_expected.not_to be_nil}
+  it {expect(:release_bike).to be_an_instance_of(Bike)}
 end
 
 
