382e788858e576efad456f90ce0789f47c498ccd
Asuka Ochi <asukaochi@gmail.com>
2016-11-09 16:42:19 +0000

    Completed exercise 11

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,6 +1,14 @@
-require 'bike.rb'
+require_relative 'bike.rb'
 class DockingStation
+
   def release_bike
      Bike.new
   end
+
+  def dock_bike
+    @bike = Bike.new
+  end
+
+attr_reader :bike
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,8 +2,23 @@ require 'docking_station'
 require 'bike'
 
 RSpec.describe DockingStation do
-it {is_expected.to respond_to(:release_bike)}
+  it {is_expected.to respond_to(:release_bike)}
+  it "responds to the method dock_bike" do
+    expect(subject).to respond_to(:dock_bike)
+  end
+  it "should return a bike when a bike was docked" do
+    bike = subject.dock_bike
+    expect(bike).to be_an_instance_of(Bike)
+  end
+  it "responds to the method bike" do
+    expect(subject).to respond_to(:bike)
+  end
 
+  it "should show the bikes in the dock when we ask to see_bikes" do
+    dock_bike = subject.dock_bike
+    bike = subject.bike
+    expect(bike).to eq(dock_bike)
+  end
 end
 
 RSpec.describe :release_bike do
@@ -11,12 +26,13 @@ RSpec.describe :release_bike do
 end
 
 RSpec.describe "Does Docking station get a bike and does it work?" do
-station = DockingStation.new
-bike = station.release_bike
-bike_status = bike.working?
-it {expect(bike).to be_an_instance_of(Bike)}
-it {expect(bike_status).to eq(true)}
+  station = DockingStation.new
+  bike = station.release_bike
+  bike_status = bike.working?
+  it {expect(bike).to be_an_instance_of(Bike)}
+  it {expect(bike_status).to eq(true)}
 end
+
 
 
 
